dbaad92d04fd5a14517e0dad4a32eb4e79fe6bb4
mavicmo <98199343+mavicmo@users.noreply.github.com>
2022-05-26 23:20:27 -0400

    Revert "Song routes"

--- server/server.js
+++ server/server.js
@@ -1,19 +1,14 @@
 //require everything
 const express = require("express");
 const cors = require("cors");
-const usersRoutes = require("./routes/users");
-const songsRoutes = require("./routes/songs");
-const app = express();
 
 //Port to run backend server
-const port = 3001 || process.env.PORT;
+const port = 3001;
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/users/", usersRoutes);
-app.use("/songs/", songsRoutes);
 
 app.listen(port, () => {
   console.log("listening on port " + port);
