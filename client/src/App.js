c043740523885e27bc2b8da8291958a193c3b703
kgarcia21 <kelvin.a.garcia21@gmail.com>
2022-05-31 21:12:32 -0400

    added some home page content, still needs more -kel

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import Login from "./components/Login.js";
+// import Login from "./components/Login.js";
 import SignUp from "./Pages/SignUp/SignUp.js";
+import { Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Navbar from "./components/NavBar/Navbar";
+
 function App() {
   return (
     <div>
       {/* <Login /> */}
-      <SignUp />
+      <Home />
     </div>
   );
 }
---
+++ client/src/Pages/Home/Home.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import * as MdIcons from "react-icons/md";
+import * as FiIcons from "react-icons/fi";
+
+function Home(props) {
+  return (
+    <>
+      <main className="main-content">
+        <div className="search">
+          <input type="text" placeholder="Search Song" id="search" />
+        </div>
+        <div className="buttons">
+          <Link to="#">
+            <button className="add-song">
+              <FiIcons.FiMusic /> Add Song
+            </button>
+          </Link>
+          <Link to="#">
+            <button className="add-playlist">
+              <MdIcons.MdOutlinePlaylistAdd /> Add Playlist
+            </button>
+          </Link>
+        </div>
+
+
+        <div className="search-result">
+            
+        </div>
+
+
+
+      </main>
+    </>
+  );
+}
+
+export default Home;
