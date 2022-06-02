427674761ef5bdacafa284bf995759b57bbfad94
Username <daniellesmikle@Danielles-Air.fios-router.home>
2022-06-01 20:43:19 -0400

    song showpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "./Pages/Login/Login.js";
 import SignUp from "./Pages/SignUp/SignUp.js";
+import Songs from "./Pages/Songs/Songs.js";
 import Home from "./Pages/Home/Home";
 import { Routes, Route } from "react-router-dom";
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" exact element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/songs" element={<Songs />} />
     </Routes>
   );
 }
